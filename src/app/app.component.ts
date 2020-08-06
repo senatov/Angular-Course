331acf96b40100d114fdd121a235196b98bc7ead
Iakov Senatov <javaEntwickler@gmail.com>
2020-08-06 07:13:11 +0200

    Lesson 15: 'Angular Injectable Services - New Section Introduction'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,27 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {COURSES} from '../db-data';
 import {Course} from './model/course';
+import {CourseCardComponent} from './course-card/course-card.component';
+import {HighlightedDirective} from './directives/highlighted.directive';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+export class AppComponent implements OnInit {
 
 
-export class AppComponent {
   courses = COURSES;
 
+  constructor() {
 
-  onCourseSelected(course: Course) {
-    console.log('onCourseSelected() - course selected', course);
   }
+
+  ngOnInit() {
+  }
+
+
+
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,41 +1,52 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {
+    AfterContentInit,
+    AfterViewInit,
+    Component,
+    ContentChildren,
+    ElementRef,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output,
+    QueryList,
+    ViewEncapsulation
+} from '@angular/core';
 import {Course} from '../model/course';
+import {CourseImageComponent} from '../course-image/course-image.component';
 
 @Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'course-card',
-  templateUrl: './course-card.component.html',
-  styleUrls: ['./course-card.component.css']
+    selector: 'course-card',
+    templateUrl: './course-card.component.html',
+    styleUrls: ['./course-card.component.css']
 })
 export class CourseCardComponent implements OnInit {
 
-  @Input()
-  course: Course;
+    @Input()
+    course: Course;
 
-  @Input()
-  cardIndex: number;
+    @Input()
+    cardIndex: number;
 
-  @Output('courseSelected')
-  courseSelected = new EventEmitter<Course>();
+    @Output('courseChanged')
+    courseEmitter = new EventEmitter<Course>();
 
-  constructor() {
-  }
 
-  ngOnInit(): void {
-  }
+    constructor() {
 
-  onCourseViewed() {
-    console.log('onCourseViewed() - \'View Course\' button  clicked');
-    this.courseSelected.emit(this.course);
-  }
-
-  isImageVisible() {
-    return this.course && this.course.iconUrl;
-  }
-
-  cardClasses() {
-    if (this.course.category === 'BEGINNER') {
-      return 'beginner';
     }
-  }
+
+    ngOnInit() {
+
+    }
+
+
+    onSaveClicked(description:string) {
+
+        this.courseEmitter.emit({...this.course, description});
+
+    }
+
+
+
+
 }
