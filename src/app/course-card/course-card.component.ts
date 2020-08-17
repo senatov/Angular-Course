c9be28f77d5964fa4a74c2b4c00ab62f00bd76f4
Iakov Senatov <javaEntwickler@gmail.com>
2020-08-17 10:28:12 +0200

    Angular Component @Output - Custom Events and Event Emitters

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,23 +1,29 @@
-import {Component, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Course} from '../model/course';
 
 @Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'course-card',
-  templateUrl: './course-card.component.html',
-  styleUrls: ['./course-card.component.css']
+    // tslint:disable-next-line:component-selector
+    selector: 'course-card',
+    templateUrl: './course-card.component.html',
+    styleUrls: ['./course-card.component.css']
 })
 export class CourseCardComponent implements OnInit {
 
-  @Input()
-  course: Course;
+    @Input()
+    course: Course;
 
+    @Output()
+    courseSelected = new EventEmitter<Course>();
 
-  constructor() {
-  }
+    constructor() {
+    }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
 
-  }
+    }
 
+    onCourseViewed() {
+        console.log('card component - button clicked!');
+        this.courseSelected.emit(this.course);
+    }
 }
