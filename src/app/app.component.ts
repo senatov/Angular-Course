db89ecab2686a64d9b29ca6640de40dd857bff02
Iakov Senatov <javaEntwickler@gmail.com>
2020-08-30 04:31:38 +0200

    View Child - Learn the multiple View Query Configuration Options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import {COURSES} from '../db-data';
 import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
 
     courses = COURSES;
