659ed971c0efe24308dc08f90136d0c4ed0804ff
Iakov Senatov <javaEntwickler@gmail.com>
2020-08-31 23:22:11 +0200

    The View Children Decorator and Query List In Detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {AfterViewInit, Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, QueryList, ViewChildren} from '@angular/core';
 import {COURSES} from '../db-data';
 import {Course} from './model/course';
+import {CourseCardComponent} from './course-card/course-card.component';
 
 
 @Component({
@@ -13,15 +14,30 @@ export class AppComponent implements AfterViewInit {
 
     courses = COURSES;
 
+    @ViewChildren(CourseCardComponent, {read: ElementRef})
+    cards: QueryList<ElementRef>;
+
     constructor() {
     }
 
     ngAfterViewInit() {
-
+        console.log(this.cards);
     }
 
     onCourseSelected(course: Course) {
     }
 
+    onCoursesEdited() {
+        this.courses.push(
+            {
+                id: 5,
+                description: 'Angular Security Course',
+                longDescription: 'Learn Web Security Fundamentals and apply them to defend an Angular / Node Application from multiple types of attacks.',
+                iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/security-cover-small-v2.png',
+                category: 'ADVANCED',
+                lessonsCount: 11
+            }
+        );
 
+    }
 }
