a5f8ae97dcb467008c54ff029d83f659ba6ff7dc
Iakov Senatov <javaEntwickler@gmail.com>
2020-10-02 07:50:43 +0200

    Introduction to Angular Attribute Directives

---
+++ src/app/directives/highlighted.directive.ts
@@ -0,0 +1,14 @@
+import {Directive} from '@angular/core';
+
+@Directive({
+    // tslint:disable-next-line:directive-selector
+    selector: '[highlighted]'
+})
+export class HighlightedDirective {
+
+    constructor() {
+        console.log('directive created!!!');
+
+    }
+
+}
