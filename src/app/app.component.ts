e50d77409481532d7db457814ca295f05bf8ecbd
Iakov Senatov <javaEntwickler@gmail.com>
2020-11-15 19:20:48 +0100

    Angular Custom Change Detection with ChangeDetectorRef

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, Inject, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {AppConfig, CONFIG_TOKEN} from './config';
 import {Course} from './model/course';
 import {CoursesService} from './services/courses.service';
@@ -6,34 +6,21 @@ import {CoursesService} from './services/courses.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit, DoCheck {
+export class AppComponent implements OnInit {
 
   courses: Course[];
-  loaded = true;
 
   constructor(private courseService: CoursesService,
-              @Inject(CONFIG_TOKEN) private config: AppConfig,
-              private cd: ChangeDetectorRef) {
+              @Inject(CONFIG_TOKEN) private config: AppConfig) {
   }
 
-  ngDoCheck(): void {
-
-    console.log("ngDoCheck()")
-    if (this.loaded) {
-      console.log("consoleAPICalled cd.markforcheck() ");
-      this.cd.markForCheck();
-      this.loaded = undefined;
-    }
-  }
 
   ngOnInit() {
     this.courseService.loadCourses().subscribe(courses => {
       this.courses = courses;
-      this.loaded = true;
     });
   }
 
